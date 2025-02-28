03ae918e4be15d7bf02fdf013833011c3c1f61c1
Petar Kabashki <peter.kabashky@gmail.com>
2025-02-28 21:49:35 +0000

    feat: modify test script to allow individual test arguments

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Allow arguments for individual tests to be passed and used. AI!
+
 eval "$(conda shell.bash hook)"
 conda activate py311
 
 # Install pytest if it's not already installed
-pip install pytest
+# pip install pytest
 
-# Pass all script arguments to pytest
-pytest test_engine.py "$@"
+# pytest test_engine.py
+pytest test_engine.py
