613e197866bf0ce8445a67bd05a0a55dbffdc196
Petar Kabashki <peter.kabashky@gmail.com>
2025-03-01 23:36:18 +0000

    chore: Clean up repository and remove workflow.png from history

--- test_engine.py
+++ test_engine.py
@@ -3,6 +3,7 @@ from engine import WFEngine
 from state_functions import StateFunctions
 import logging
 
+# The tests should take into account that the engine is a generator that yields it's current state AI!
 # Helper functions for tests
 def get_log_contents(test_name):
     with open(f'engine_log.txt', 'r') as f:
